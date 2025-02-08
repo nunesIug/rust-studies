b2e96523e847d02be2d5df64229d3ae799ad75d0
GuilhermeNunes <digobros13@gmail.com>
2025-02-08 15:52:13 -0300

    initial

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,20 @@
 fn main() {
-    let mut username: &str = "Nu S";
+    let username: &str = "Nu S";
 
-    username = "Guilherme";
-
-    let playing: bool = false;
+    // user info
+    let playing: bool = true;
     let online: bool = false;
-    let game: &str = "Red Dead Redemption II";
 
+    let game_library: [&str; 2] = ["Red dead Redemption II", "Resident Evil II"];
+
+    let time_played: (u32, u32, u32) = (10, 13, 15);
+
+    
+    // verify if player is online and playing
     if playing && online == true {
-        println!("{} is online, playing {}.", username, game);
+        return println!("{} is online, playing {}", username, game_library[0]);
     }
     else {
-        println!("{} is offline, last game he played was {}", username, game);
-    }
+        return println!("{} is offline, the last game he played was {}. He played for {} hours.", username, game_library[0], time_played.2);
+    };
 }
