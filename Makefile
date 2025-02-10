b6461a7c7d2f13b5bb14a930f0eca3226bb31418
GuilhermeNunes <digobros13@gmail.com>
2025-02-10 00:05:54 -0300

    commit

--- Makefile
+++ Makefile
@@ -2,3 +2,11 @@ all:
 
 cargo: 	
 	cargo watch -x run 
+
+build: 
+	cargo build
+
+
+# buildar em release para diminuir o tamanho do binário do arquivo.
+release:	
+	cargo build --release 
--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,24 @@
+use std::io;
 fn main() {
-    let username: &str = "Nu S";
 
-    // user info
-    let playing: bool = true;
-    let online: bool = false;
+    println!("player, enter ur name: ");
 
-    let game_library: [&str; 2] = ["Red dead Redemption II", "Resident Evil II"];
-
-    let time_played: (u32, u32, u32) = (10, 13, 15);
-
+    let mut username: String = String::new();
     
-    // verify if player is online and playing
-    if playing && online == true {
-        return println!("{} is online, playing {}", username, game_library[0]);
-    }
+    let online: bool = true;
+    let playing: bool = true;
+    let favorite_games: [&str; 3] = ["Red dead Redemption II", "Dying light", "Residente Evil II"];
+
+
+    io::stdin()
+        .read_line(&mut username)
+        .expect("an error accoured.");
+
+
+    if online && playing == true {
+        return println!("{} is online, playing {:?}", username, favorite_games[0]);
+    } 
     else {
-        return println!("{} is offline, the last game he played was {}. He played for {} hours.", username, game_library[0], time_played.2);
-    };
+        return println!("{} is offline, the last game he played was {}", username, favorite_games[2]);
+    }
 }
