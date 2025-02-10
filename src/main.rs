70035db2bf1ec600ba3481a6f6706cf9fb70be47
GuilhermeNunes <digobros13@gmail.com>
2025-02-10 17:50:24 -0300

    initial commit

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,32 @@
 use std::io;
 fn main() {
 
-    println!("player, enter ur name: ");
+    let error: &str = "An error accoured.";
 
-    let mut username: String = String::new();
-    
-    let online: bool = true;
-    let playing: bool = true;
-    let favorite_games: [&str; 3] = ["Red dead Redemption II", "Dying light", "Residente Evil II"];
+    println!("be welcome player, enter ur name to continue:" );
 
+    let mut player_name: String = String::new();
 
     io::stdin()
-        .read_line(&mut username)
-        .expect("an error accoured.");
+        .read_line(&mut player_name)
+        .expect(error);
+
+
+    // this code repeat: -, 20 times
+    println!("{:-^40}", "welcome to your account info");
+
+
+    let online: bool = false;
+    let playing: bool = true;
+    
+    let favorite_games: [&str; 3] = ["Red dead Redemption II", "Dying light", "Resident Evil II"];
 
 
     if online && playing == true {
-        return println!("{} is online, playing {:?}", username, favorite_games[0]);
-    } 
-    else {
-        return println!("{} is offline, the last game he played was {}", username, favorite_games[2]);
+        return println!("{} is online playing {}", player_name.to_uppercase(), favorite_games[0]);
     }
+    else {
+        return println!("{} is offline, the last game he played was {}", player_name.to_uppercase(), favorite_games[2]);
+    };
+
 }
