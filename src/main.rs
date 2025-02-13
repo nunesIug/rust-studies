3d00fdf48b4cbbb641a616e78f5d4cb1ff393ea2
GuilhermeNunes <digobros13@gmail.com>
2025-02-13 00:54:13 -0300

    just to practice

--- src/main.rs
+++ src/main.rs
@@ -1,41 +1,49 @@
 use std::io;
 
-fn main () {
-
-    println!("{:-^40}", "enter ur data, player: ");
+fn main() {
 
     let mut username: String = String::new();
-    let mut email: String = String::new();
-    let mut password: String = String::new();
-
-
 
     io::stdin()
         .read_line(&mut username)
         .expect("an error accoured.");
 
-    io::stdin()
-        .read_line(&mut email)
-        .expect("an error accoured.");
 
-    io::stdin()
-        .read_line(&mut password)
-        .expect("an error accoured.");
+    if username.trim().to_lowercase() != "nu s" {
+        return println!("sorry {}, but ur credentials r invalid. Try again.", username);
+    };
 
 
-
-    fn info_player (username: String, email: String, password: String, authenticated: bool) {
-        return println!("Info player account: \n username: {} \n email: {} \n password: {} \n is authenticated: {}", username, email, password, authenticated);
+    struct Player <'a> {
+        username: String,
+        skills: u32,
+        online: bool,
+        playing: bool,
+        favorite_games: [&'a str; 3],
+        game_library: [&'a str; 5]
     }
-    
 
 
-    let info: () = info_player(username, email, password, true);
+    let player: Player = Player {
+        username: username,
+        skills: 0,
+        online: online(),
+        playing: true,
+        favorite_games: ["Red dead Redemption II", "Dying light", "RE II Remake"],
+        game_library: ["Red dead Redemption II", "Dying light", "RE II Remake", "Minecraft", "RE IV Remake"]
+    };
 
 
-    let favorite_games: [&str; 3] = ["Red dead Redemption II", "Resident Evil II Remake", "Dying light"];
+    if player.online == false {
+        return println!("{} is offline, the last game played was {}", player.username, player.favorite_games[0]);
+    }
 
 
-    println!("{:?} \n favorite games: \n {:?} \n last game played: {:?}", info, favorite_games, favorite_games[2]);
+    println!("Hello, player. welcome {}", player.username);
+    println!("here's ur account informations.. \n skills: {} \n is online: {} \n is playing: {} \n ur game library: {:?} \n ur favorite games: {:?}", player.skills, player.online, player.playing, player.game_library, player.favorite_games);
 
+}
+
+fn online() -> bool {
+    return true;
 }
